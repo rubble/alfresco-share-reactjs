87a33ba2a6502968762e8cf8ef739a42f05644d9
Emlyn Clay <eclay101@gmail.com>
2016-04-29 16:03:42 +0200

    Import JSX components, instantiate JSX table and render.

--- share-reactjs/share-reactjs-share-amp/src/main/amp/web/js/react-widget/dashlets/widgets/ReactWidget.js
+++ share-reactjs/share-reactjs-share-amp/src/main/amp/web/js/react-widget/dashlets/widgets/ReactWidget.js
@@ -4,9 +4,10 @@ define(["dojo/_base/declare",
         "dijit/_TemplatedMixin",
         "dojo/text!./templates/ReactWidget.html",
         "react",
-        "react-dom"
+        "react-dom",
+        "./JSXComponents"
     ],
-    function(declare, _Widget, Core, _Templated, template, React, ReactDOM) {
+    function(declare, _Widget, Core, _Templated, template, React, ReactDOM, Components) {
         
         // Your React classes go here
         var Hello = React.createClass({
@@ -15,19 +16,28 @@ define(["dojo/_base/declare",
                 return React.createElement("div", null, "Hello ", this.props.name);
             }
         });
-        
+
+        var start = new Date();
+        var Timer = React.createFactory(Components.Timer);
+        var Table = React.createFactory(Components.Table);
+
         return declare([_Widget, Core, _Templated], {
             templateString: template,
             i18nRequirements: [ {i18nFile: "./i18n/ReactWidget.properties"} ],
-            cssRequirements: [{cssFile:"./css/ReactWidget.css"}],
+            cssRequirements: [
+                {cssFile:"./css/font-awesome.min.css"},
+                {cssFile:"./css/semantic.css"},
+                {cssFile:"./css/ReactWidget.css"}
+                ],
             
             buildRendering: function reactwidgets_widgets_TemplateWidget__buildRendering() {
                 this.inherited(arguments);
             },
             
             postCreate: function() {
-                ReactDOM.render(
-                    React.createElement(Hello, {name: "World"}),
+                // Mount the JSX component in the app container
+                React.render(
+                    Table(),
                     document.getElementById('app')
                 );
             }
